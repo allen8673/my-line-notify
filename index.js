88971610cef6aa94d5961922d27c841a5f6ef9e7
WHQ\10704306 <Allen_CL_Lin@wistron.com>
2019-10-31 11:04:03 +0800

    aaaa

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const PORT = 8888;
-const HOST = '0.0.0.0';
+// const PORT = 8888;
+// const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
    res.sendfile('index.html', function(err) {
         if (err) res.send(404);
     });
 });
-app.listen(PORT, HOST);
+// app.listen(PORT, HOST);
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
